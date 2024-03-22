dde2b4e9bdfa1404a6efbd67c720704bbf454236
Trupti96-web <145888427+Trupti96-web@users.noreply.github.com>
2024-03-22 22:48:04 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,24 @@
+//var a=5;
+//console.log(a);
+
+/* console.log(a);
+var a;
+
+add()
+
+function add(){
+    console.log("trupti")
+} 
+*/
+
+
+function fun1(){
+    let a = 2;
+
+    function fun2(){
+        console.log(a)
+    }
+    a=8;
+   return fun2();
+}
+ fun1();
